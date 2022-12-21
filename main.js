f0a32c491f7ac21c2dc919f2ae85ac2ff91b030c
SeungJin051 <83889135+SeungJin051@users.noreply.github.com>
2022-12-22 00:21:25 +0900

    add modal-team JS : SeungJin051

--- main.js
+++ main.js
@@ -11,3 +11,11 @@ mainBtn.addEventListener("click", removeMain);
 function gamePage() {
   window.location.href = "game.html";
 }
+
+$(".main-team").click(function () {
+  $(".modal").fadeIn();
+});
+
+$(".modal-close").click(function () {
+  $(".modal").fadeOut();
+});
