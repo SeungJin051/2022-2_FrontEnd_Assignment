2422c67a31b1df1d8a3c4ad5ca286914ac2b6323
SeungJin051 <83889135+SeungJin051@users.noreply.github.com>
2022-12-22 00:37:04 +0900

    URL 에러 : SeungJin051

--- main.js
+++ main.js
@@ -1,24 +1,23 @@
 let mainBtn = document.querySelector(".main-btn");
-let main = document.querySelector(".main-container");
 let quizBox = document.querySelector(".quiz-container");
-window.onload = function () {
-  function removeMain() {
-    $(main).fadeOut(400);
-    setTimeout(gamePage, 450);
-  }
 
-  mainBtn.addEventListener("click", removeMain);
+function removeMain() {
+  let main = document.querySelector(".main-container");
+  $(main).fadeOut(400);
+  setTimeout(gamePage, 450);
+}
 
-  function gamePage() {
-    window.location.href =
-      "https://seungjin051.github.io/2022-2_FrontEnd_Assignment/game.html";
-  }
+mainBtn.addEventListener("click", removeMain);
 
-  $(".main-team").click(function () {
-    $(".modal").fadeIn();
-  });
+function gamePage() {
+  window.location.href =
+    "https://seungjin051.github.io/2022-2_FrontEnd_Assignment/game.html";
+}
 
-  $(".modal-close").click(function () {
-    $(".modal").fadeOut();
-  });
-};
+$(".main-team").click(function () {
+  $(".modal").fadeIn();
+});
+
+$(".modal-close").click(function () {
+  $(".modal").fadeOut();
+});
