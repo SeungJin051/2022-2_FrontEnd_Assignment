62aa20d200f643e1bb80e0bcda9d90f9a97b4cdf
SeungJin051 <83889135+SeungJin051@users.noreply.github.com>
2022-12-22 01:49:34 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
 window.onload = function () {
   let mainBtn = document.querySelector(".main-btn");
-  let main = document.querySelector(".main-container");
 
   function gamePage() {
     window.location.href =
       "https://seungjin051.github.io/2022-2_FrontEnd_Assignment/game.html";
   }
 
-  function removeMain() {
+  // function removeMain() {
+  //   $(main).fadeOut(400);
+  //   setTimeout(gamePage, 450);
+  // }
+
+  $(mainBtn).click(function removeMain() {
+    let main = document.querySelector(".main-container");
     $(main).fadeOut(400);
     setTimeout(gamePage, 450);
-  }
-
-  mainBtn.addEventListener("click", removeMain);
+  });
 
   $(".main-team").click(function () {
     $(".modal").fadeIn();
